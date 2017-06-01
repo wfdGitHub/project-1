cedbf54710909a23ca391d8ce1a6bf17df0c7072
yywl-wfd <752387064@qq.com>
2017-06-01 16:11:58 +0800

    更新

--- server/game-server/app/games/NiuNiu.js
+++ server/game-server/app/games/NiuNiu.js
@@ -115,7 +115,7 @@ module.exports.createRoom = function(roomId,channelService,cb) {
       //   banker = roomHost
       //   maxBet = 10
       // }
-      if(room.gameMode == MODE_GAME_SHIP || room.gameMode == MODE_GAME_BULL){
+      if(room.gameMode == MODE_GAME_SHIP){
         room.bankerMode = MODE_BANKER_NONE
       }
       room.join(uid,sid,{ip : param.ip},cb)
