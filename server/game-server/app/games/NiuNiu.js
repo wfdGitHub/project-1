9c94e0fe9d36d1e201bab8159f75ffd50484bc2f
yywl-wfd <752387064@qq.com>
2017-06-01 15:10:13 +0800

    修复

--- server/game-server/app/games/NiuNiu.js
+++ server/game-server/app/games/NiuNiu.js
@@ -857,7 +857,7 @@ var copyObj = function(obj) {
 var deepCopy= function(source) { 
   var result={}
   for (var key in source) {
-        result[key] = typeof source[key]===’object’? deepCoyp(source[key]): source[key]
+        result[key] = typeof source[key]==="object"? deepCoyp(source[key]): source[key]
      } 
      return result;
 }
