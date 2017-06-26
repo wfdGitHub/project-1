86ae7afa8c5a4de1d3790937c96c0cf703215527
yywl-wfd <752387064@qq.com>
2017-06-26 12:02:47 +0800

    微信不能识别字符替换

--- server/game-server/app/servers/connector/handler/entryHandler.js
+++ server/game-server/app/servers/connector/handler/entryHandler.js
@@ -217,6 +217,7 @@ handler.enter = function(msg, session, next) {
         self.app.rpc.db.remote.getPlayerInfo(session,userId,function(data) {
           notify.cmd = "userInfo"
           notify.data = data
+          notify.data.nickname = strReplace(notify.data.nickname)
           notify.openId = openId
           notify.unionid = userId
           if(data.freeze == 1){
@@ -362,3 +363,38 @@ var sendHttp = function(notify) {
 function md5 (text) {
   return crypto.createHash('md5').update(text).digest('hex');
 };
+
+
+
+var strReplace = function(str) {
+  var strArr = str.split(""),
+      result = "",
+      totalLen = 0;
+
+  for(var idx = 0; idx < strArr.length; idx ++) {
+      // 超出长度,退出程序
+      if(totalLen >= 16) break;
+      var val = strArr[idx];
+      // 英文,增加长度1
+      if(/[a-zA-Z]/.test(val)) {
+          totalLen = 1 + (+totalLen);
+          result += val;
+      }
+      // 中文,增加长度2
+      else if(/[\u4e00-\u9fa5]/.test(val)) {
+          totalLen = 2 + (+totalLen);
+          result += val;
+      }
+      // 遇到代理字符,将其转换为 "口", 不增加长度
+      else if(/[\ud800-\udfff]/.test(val)) {
+          // 代理对长度为2,
+          if(/[\ud800-\udfff]/.test(strArr[idx + 1])) {
+              // 跳过下一个
+              idx ++;
+          }
+          // 将代理对替换为 "口"
+          result += "口";
+      }
+  }
+  return result
+}
