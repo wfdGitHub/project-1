7a6271e6c67771a98e8a9e9ac61cc9b883349902
yywl-wfd <752387064@qq.com>
2017-06-30 18:15:47 +0800

    取消炸金牛庄家计数

--- server/game-server/app/games/ZhaJinNiu.js
+++ server/game-server/app/games/ZhaJinNiu.js
@@ -702,7 +702,6 @@ var MING_CARD_NUM = 3               //明牌数量
     local.settlement = function() {
       if(gameState !== conf.GS_SETTLEMENT){
         room.runCount++
-        player[banker].bankerCount
          clearTimeout(timer)
          gameState = conf.GS_SETTLEMENT
         //console.log("settlemnt")
