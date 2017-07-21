9c7c5ccbb22cedfcc33815affe97eea28a8739f0
yywl-wfd <752387064@qq.com>
2017-07-21 14:23:27 +0800

    版本号修改

--- server/game-server/app/servers/connector/handler/entryHandler.js
+++ server/game-server/app/servers/connector/handler/entryHandler.js
@@ -16,7 +16,7 @@ var Handler = function(app) {
 
 var handler = Handler.prototype
 
-var version = "1.1.0&QWEOJZXKLCNEUIOGOFJGFIOVBFDKOPF"
+var version = "1.2.024&KSAJDGFKJASHDFGASHDGAFDGH"
 //获取公告
 handler.getNotify = function(msg,session,next) {
   var self = this
