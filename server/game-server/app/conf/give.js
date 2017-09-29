e5f75846537e5a80e23b8e0be10fc928c390c92f
yywl-wfd <752387064@qq.com>
2017-09-29 10:50:12 +0800

    赠送配置修改

--- server/game-server/app/conf/give.js
+++ server/game-server/app/conf/give.js
@@ -1,11 +1,10 @@
 //赠送
 module.exports = 
 {
-	"1" : {"name" : "招财猫","gold" : 1000000,"charm" : 100,"needGold" : 2000},
-	"2" : {"name" : "叮当","gold" : 600000,"charm" : 50,"needGold" : 1200},
-	"3" : {"name" : "鸡尾酒","gold" : 300000,"charm" : 10,"needGold" : 600},
-	"4" : {"name" : "玫瑰","gold" : 200000,"charm" : 5,"needGold" : 400},
-	"5" : {"name" : "鸡腿","gold" : 100000,"charm" : 2,"needGold" : 200},
-	"6" : {"name" : "汉堡","gold" : 50000,"charm" : 1,"needGold" : 100},
-	"7" : {"name" : "便便","gold" : 10000,"charm" : -1,"needGold" : 20},
+	"1" : {"name" : "招财猫","gold" : 5000000,"charm" : 100},
+	"2" : {"name" : "叮当","gold" : 1000000,"charm" : 50},
+	"3" : {"name" : "鸡尾酒","gold" : 500000,"charm" : 10},
+	"4" : {"name" : "玫瑰","gold" : 100000,"charm" : 5},
+	"5" : {"name" : "鸡腿","gold" : 50000,"charm" : 2},
+	"6" : {"name" : "汉堡","gold" : 10000,"charm" : 1},
 }
