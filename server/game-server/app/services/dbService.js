f2f2ade840ff44fc3aa955292cac7cd2a3293708
yywl-wfd <752387064@qq.com>
2017-10-09 18:48:36 +0800

    数据库

--- server/game-server/app/services/dbService.js
+++ server/game-server/app/services/dbService.js
@@ -13,31 +13,37 @@ var dbService = function(app) {
 
 dbService.prototype.start = function(cb){
 	var db = redis.createClient(RDS_PORT,RDS_HOST,RDS_OPTS)
-	this.app.set("dbService",dbService)
-	db.on("ready",function(res) {
-		dbService.db = db
-		//数据库初始配置
-		dbService.db.get("nn:acc:lastid",function(err,data) {
-			if(data === null){
-		        console.log("\033[33m[INFO] DataBase check - nn:acc:lastid\033[0m");
-		        db.set("nn:acc:lastid",10000);
-    		}
+	var self = this
+	db.select("1",function(err) {
+		if(err){
+			console.log(err)
+		}
+		self.app.set("dbService",dbService)
+		db.on("ready",function(res) {
+			dbService.db = db
+			//数据库初始配置
+			dbService.db.get("nn:acc:lastid",function(err,data) {
+				if(data === null){
+			        console.log("\033[33m[INFO] DataBase check - nn:acc:lastid\033[0m");
+			        db.set("nn:acc:lastid",10000);
+	    		}
+			})
+			dbService.db.get("nn:acc:addDiamond",function(err,data) {
+				if(data === null){
+			        console.log("\033[33m[INFO] DataBase check - nn:acc:addDiamond\033[0m");
+			        db.set("nn:acc:addDiamond",0);
+	    		}
+			})
+			dbService.db.get("nn:notifys",function(err,data) {
+				if(data === null){
+			        console.log("\033[33m[INFO] DataBase check - nn:notifys\033[0m");
+			        var notify = {"1" : {"name" : "","content" : ""}}
+			        db.set("nn:notifys",JSON.stringify(notify));
+	    		}
+			})
 		})
-		dbService.db.get("nn:acc:addDiamond",function(err,data) {
-			if(data === null){
-		        console.log("\033[33m[INFO] DataBase check - nn:acc:addDiamond\033[0m");
-		        db.set("nn:acc:addDiamond",0);
-    		}
-		})
-		dbService.db.get("nn:notifys",function(err,data) {
-			if(data === null){
-		        console.log("\033[33m[INFO] DataBase check - nn:notifys\033[0m");
-		        var notify = {"1" : {"name" : "","content" : ""}}
-		        db.set("nn:notifys",JSON.stringify(notify));
-    		}
-		})
+		cb()
 	})
-	cb()
 }
 dbService.updateDiamond = function(value) {
 	var cmd = "nn:acc:addDiamond"
