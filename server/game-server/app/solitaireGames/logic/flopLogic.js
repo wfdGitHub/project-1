39eacaf2a4e6d4262e9bd3884264b7a004f9af8a
yywl-wfd <752387064@qq.com>
2017-11-09 19:27:11 +0800

    牌型算法

--- server/game-server/app/solitaireGames/logic/flopLogic.js
+++ server/game-server/app/solitaireGames/logic/flopLogic.js
@@ -11,6 +11,22 @@ var COMB_TYPE_OX9   =    9           // 同花大顺   250
 var COMB_TYPE_OX10  =    10          // 五条       750
 var awardList = [0,1,2,3,5,7,10,60,150,250,750]
 
+var CARD_VALUE = {
+  "1" : 14,
+  "2" : 2,
+  "3" : 3,
+  "4" : 4,
+  "5" : 5,
+  "6" : 6,
+  "7" : 7,
+  "8" : 8,
+  "9" : 9,
+  "10" : 10,
+  "11" : 11,
+  "12" : 12,
+  "13" : 13,
+  "20" : 20
+}
 //洗牌
 module.exports.shuffle = function() {
 	var cards = {}					//牌组
@@ -20,8 +36,8 @@ module.exports.shuffle = function() {
 		 	cards[cardCount++] = {num : i,type : j}
 		}
 	}
-	cards[cardCount++] = {num : 20,type : 0}
-	cards[cardCount++] = {num : 20,type : 1}
+	cards[cardCount++] = {num : 20,type : 5}
+	cards[cardCount++] = {num : 20,type : 6}
     //洗牌
     for(var i = 0;i < cardCount;i++){
     	var tmpIndex = Math.floor(Math.random() * (cardCount - 0.000001))
@@ -34,8 +50,134 @@ module.exports.shuffle = function() {
 
 
 //获取牌型
-module.exports.getType = function(handCard){
-	
+module.exports.getType = function(hand){
+  var cardType = {
+    "type" : 0,
+    "award" : 0
+  }
+
+  var handCard = deepCopy(hand)
+  //排序
+  for(var i = 0;i < 5 - 1;i++){
+    for(var j = i + 1; j < 5;j++){
+      if(CARD_VALUE[handCard[i].num] > CARD_VALUE[handCard[j].num] || (CARD_VALUE[handCard[i].num] == CARD_VALUE[handCard[j].num] && handCard[i].type > handCard[j].type)){
+        var tmpCard = deepCopy(handCard[i])
+        handCard[i] = deepCopy(handCard[j])
+        handCard[j] = tmpCard
+      }
+    }
+  }
+
+  //同花标志
+  var tongHuaFlag = false
+  if(handCard[0].type == handCard[1].type && handCard[1].type == handCard[2].type && handCard[2].type == handCard[3].type && handCard[3].type == handCard[4].type){
+    tongHuaFlag = true
+  }
+  //顺子标志
+  var shunFlag = false
+  if(CARD_VALUE[handCard[0].num] == CARD_VALUE[handCard[1].num] - 1 && CARD_VALUE[handCard[1].num] == CARD_VALUE[handCard[2].num] - 1 && 
+  	CARD_VALUE[handCard[2].num] == CARD_VALUE[handCard[3].num] - 1 && CARD_VALUE[handCard[3].num] == CARD_VALUE[handCard[4].num] - 1){
+    shunFlag = true
+  }
+  //统计各个牌型数量
+  var cardList = {}
+  for(var i = 1;i <= 13;i++){
+  	cardList[i] = 0
+  }
+  cardList[20] = 0
+  for(var i = 0;i < 5;i++){
+  	cardList[handCard[i].num]++
+  }
+  var wangCount = cardList[20]
+  //五条
+  for(var i = 1;i <= 13;i++){
+    if(cardList[i] + wangCount == 5){
+      cardType.type = COMB_TYPE_OX10
+      cardType.award = awardList[cardType.type]
+      return cardType
+    }
+  }
+  //同花大顺
+  if(shunFlag && tongHuaFlag){
+    if(CARD_VALUE[handCard[0].num] == 10 && CARD_VALUE[handCard[4].num] == 14){
+      cardType.type = COMB_TYPE_OX9
+      cardType.award = awardList[cardType.type]
+      return cardType      
+    }else{
+      //同花顺
+      cardType.type = COMB_TYPE_OX8
+      cardType.award = awardList[cardType.type]
+      return cardType        
+    }
+  }
+  //四条
+  for(var i = 1;i <= 13;i++){
+    if(cardList[i] + wangCount == 4){
+      cardType.type = COMB_TYPE_OX7
+      cardType.award = awardList[cardType.type]
+      return cardType
+    }
+  }
+  //葫芦
+  var thereFlag = 0
+  var twoFlag = 0
+  for(var i = 1;i <= 13;i++){
+    if(cardList[i] == 3){
+      thereFlag++
+    }
+    if(cardList[i] == 2){
+      twoFlag++
+    }
+  }
+  if(thereFlag > 0 && twoFlag > 0){
+    cardType.type = COMB_TYPE_OX6
+    cardType.award = awardList[cardType.type]
+    return cardType    
+  }
+  //同花
+  if(tongHuaFlag){
+    cardType.type = COMB_TYPE_OX5
+    cardType.award = awardList[cardType.type]
+    return cardType
+  }
+  //顺子
+  if(shunFlag){
+    cardType.type = COMB_TYPE_OX4
+    cardType.award = awardList[cardType.type]
+    return cardType
+  }
+  //三条
+  if(thereFlag > 0){
+    cardType.type = COMB_TYPE_OX3
+    cardType.award = awardList[cardType.type]
+    return cardType    
+  }
+  //两对
+  if(twoFlag == 2){
+    cardType.type = COMB_TYPE_OX2
+    cardType.award = awardList[cardType.type]
+    return cardType    
+  }
+  //一对8以上
+  if(twoFlag > 0){
+    var tmpFlag = false
+    for(var i = 8;i <= 13;i++){
+      if(cardList[i] == 2){
+        tmpFlag = true
+      }
+    }
+    if(cardList[1] == 2){
+      tmpFlag = true
+    }
+    if(tmpFlag){
+      cardType.type = COMB_TYPE_OX1
+      cardType.award = awardList[cardType.type]
+      return cardType
+    }
+  }
+  cardType.type = COMB_TYPE_OX0
+  cardType.award = awardList[cardType.type]
+  return cardType
 }
 
 
@@ -43,3 +185,16 @@ module.exports.getType = function(handCard){
 
 
 //换差牌
+
+
+
+
+
+
+var deepCopy = function(source) { 
+  var result={}
+  for (var key in source) {
+        result[key] = typeof source[key]==="object"? deepCopy(source[key]): source[key]
+     } 
+  return result;
+}
