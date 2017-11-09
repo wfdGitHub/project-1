37e7063ed0783a2d50a8b025c898c6eb11ad9f69
yywl-wfd <752387064@qq.com>
2017-11-09 14:32:21 +0800

    更新

--- server/game-server/app/conf/niuniuConf.js
+++ server/game-server/app/conf/niuniuConf.js
@@ -51,6 +51,11 @@ conf.MODE_GAME_BULL   = 3              //斗公牛模式
 conf.MODE_GAME_SHIP   = 4              //开船模式
 conf.MODE_GAME_ZHAJINNIU = 5		   //炸金牛
 conf.MODE_GAME_CRAZE = 6 			   //疯狂模式
+conf.MODE_GAME_FLOP = 7 			   //翻牌机
+
+
+
+
 //定庄模式
 conf.MODE_BANKER_ROB   = 1              //随机抢庄
 conf.MODE_BANKER_HOST  = 2              //房主做庄
